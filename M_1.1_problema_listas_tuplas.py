f3565cd8539dd4f53fd4b0264627e37655e06074
Alets125 <jesusalexis-115@hotmail.com>
2025-02-11 19:55:05 -0800

    Probando def puestas

---
+++ M_1.1_problema_listas_tuplas.py
@@ -0,0 +1,12 @@
+#Garnica Mendoza Jesus Alexis
+#Grupo 951
+#11/feb/2025
+#Descripcion del Problema (Para despues)
+
+
+def estadistica():
+    pass
+
+
+if __name__ == "__main__":
+    estadistica()
